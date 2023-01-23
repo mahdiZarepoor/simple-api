f098d950c46c1351a7cbd6eb9eb8cb6471011dd0
Mahdi Zarepoor <mahdi.dev.contact@gmail.com>
2023-01-23 14:58:59 +0330

    some refactoring

--- main.go
+++ main.go
@@ -3,22 +3,19 @@ package main
 import (
 	"os"
 
-	"github.com/mahdiZarepoor/simple-api/handlers"
-	_ "github.com/mahdiZarepoor/simple-api/handlers"
+	"github.com/mahdiZarepoor/simple-api/app"
 )
 
 func main() {
 
-	a := handlers.App{} 
+	a := app.App{}
 
-	// before calling os.Getenv we have to set these environment variables 
-	// for our connection to database 
+	// before calling os.Getenv we have to set these environment variables
+	// for our connection to database
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"))
 
 	a.Run(":8000")
-}	
-
-
+}
